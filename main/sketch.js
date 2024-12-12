fbe89f66c46db8799b163ee04a15a0f53de783d3
Chavali V S S Chaitanya <55374599+cvsschaitanya@users.noreply.github.com>
2024-12-12 21:45:47 +0000

    Add colors and  random start

--- main/sketch.js
+++ main/sketch.js
@@ -1,30 +1,32 @@
 const particles = [];
 function setup() {
     createCanvas(500, 500);
-    const n = 1;
+    const n = 100;
     for (let i = 1; i <= n; i++) {
         particles.push(new Particle(i, 5, {
-            pos: createVector(i*20, 20),
-            acc: createVector(5, 0),
+            pos: createVector(random(width), random(height)),
+            vel: p5.Vector.random2D(),
+            acc: p5.Vector.random2D(),
         }));
     }
 }
 
 function draw() {
-    background(220);
+    background(255);
     noFill();
     const wallThickness = 1;
+    stroke(0);
     strokeWeight(wallThickness);
     rect(0, 0, width, height);
 
-    let gravity = createVector(0, 0.75);
+    let gravity = createVector(0, .75);
     particles.forEach(p => {
         p.show();
         p.update();
         p.applyForce(gravity);
         p.collideBox(0 + (p.getRadius() + wallThickness), width - (p.getRadius() + wallThickness), 0 + (p.getRadius() + wallThickness), height - (p.getRadius() + wallThickness),);
     });
-    
+
 }
 
 
