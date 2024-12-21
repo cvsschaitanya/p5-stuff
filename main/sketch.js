7a706caabb97a1303fc96784f85415cc877c3370
Chavali V S S Chaitanya <55374599+cvsschaitanya@users.noreply.github.com>
2024-12-21 16:11:20 +0000

    Fix speed and count

--- main/sketch.js
+++ main/sketch.js
@@ -1,7 +1,7 @@
 const particles = [];
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    const n = 250;
+    const n = 150;
     for (let i = 1; i <= n; i++) {
         particles.push(new Particle(i, random(5, 50), {
             pos: createVector(random(width), random(height/2)),
